3f2878ddfd5f1116abe8a1f7ae71f023f4c7765d
Alin Jerpelea <jerpelea@gmail.com>
2013-05-23 17:56:00 +0300

    montblanc: remove p2p
    Change-Id: Id10ac0667077fdc25794a30c8a9c641658099fc6
    
    Conflicts:
    	montblanc.mk

--- montblanc.mk
+++ montblanc.mk
@@ -9,6 +9,7 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/montblanc-common/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
+<<<<<<< HEAD
     frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
